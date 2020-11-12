339fc139a866341b51092229e6431771c260bce3
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-13 02:52:08 +0300

    define user class objects

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    avatar_url:any;
+    name:any;
+    company:any;
+    location:any;
+    followers:any;
+    following:any;
+    logged_at:Date;
+    email:string;
+    bio:any;
+    login:any;
 }
