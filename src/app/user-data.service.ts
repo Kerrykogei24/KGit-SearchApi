9320b4091c3592d9031dab7d9102726f48ba17d9
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-13 03:22:57 +0300

    add function in services

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -1,9 +1,35 @@
+import { Repos } from './repos';
+import { User } from './user';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { promise } from 'protractor';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
+user:User[]=[];
+repo:Repos[]=[];
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  searchUser(searchTerm:string){
+    interface data{
+      login:string;
+      avatar_url:string;
+      following :string;
+      followings :string;
+      public_repos:string;
+
+    }
+
+
+
+    return new Promise((resolve,reject)=>{
+      this.user=[];
+
+      this.http.get<data>(this.)
+    })
+  }
 }
