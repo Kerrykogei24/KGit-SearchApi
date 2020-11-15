58d2c18fc68864911978e9ae7371b94dcd8f9209
Kerrykogei24 <kerrykomar.gmail.com>
2020-11-15 15:04:54 +0300

    style user profile

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -4,18 +4,17 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-    constructor(el: ElementRef) {
-       el.nativeElement.style.backgroundColor = 'yellow';
-    }
-    @HostListener('mouseenter') onMouseEnter() {
-      this.highlight('yellow');
-    }
-    
-    @HostListener('mouseleave') onMouseLeave() {
-      this.highlight(null);
-    }
-    
-    private highlight(color: string) {
-      this.el.nativeElement.style.backgroundColor = color;
-    }
+  constructor(private el: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('#5D6D7E ');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 }
